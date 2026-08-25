f23b7f08924c8bea35b6787e5766d568df35f602
piyawatthas <piyawat.thas@bumail.net>
2026-08-25 10:39:31 +0700

    Add Lab02

---
+++ Lab02/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Student ID :
+ * Name       :
+ * Section    :
+ * No.        :
+ * Course     : GI113 Computer Programming (GI)
+ */
+
+namespace Lab02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
